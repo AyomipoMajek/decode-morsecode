58a152f8b363557c42904677b0e4d4914fe91ab8
Ayomipo Majek <ayomipomajek@gmail.com>
2023-05-17 22:02:27 +0100

    merge conflict fixed

---
+++ morse_decoder.rb
@@ -0,0 +1,60 @@
+@morse_code_alphabet = {
+  'A' => '.-',
+  'B' => '-...',
+  'C' => '-.-.',
+  'D' => '-..',
+  'E' => '.',
+  'F' => '..-.',
+  'G' => '--.',
+  'H' => '....',
+  'I' => '..',
+  'J' => '.---',
+  'K' => '-.-',
+  'L' => '.-..',
+  'M' => '--',
+  'N' => '-.',
+  'O' => '---',
+  'P' => '.--.',
+  'Q' => '--.-',
+  'R' => '.-.',
+  'S' => '...',
+  'T' => '-',
+  'U' => '..-',
+  'V' => '...-',
+  'W' => '.--',
+  'X' => '-..-',
+  'Y' => '-.--',
+  'Z' => '--..',
+  '0' => '-----',
+  '1' => '.----',
+  '2' => '..---',
+  '3' => '...--',
+  '4' => '....-',
+  '5' => '.....',
+  '6' => '-....',
+  '7' => '--...',
+  '8' => '---..',
+  '9' => '----.'
+}
+
+
+def decode_char(morse_letter)
+  @morse_code_alphabet.key(morse_letter)
+end
+
+def decode_word(morse_word)
+  morse_chars = morse_word.split
+  english_word = ''
+  morse_chars.each do |char|
+    puts(char)
+    english_word += decode_char(char)
+  end
+  english_word
+end
+
+def decode_message(message)
+  words = message.split('  ')
+  words.map { |word| decode_word(word) }.join(' ')
+end
+
+puts decode_message('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
